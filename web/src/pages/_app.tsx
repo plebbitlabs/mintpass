2394f80bbe158ba659eb085dc13f6ec341e69b0d
plebeius <tom@plebbit.com>
2025-09-25 13:03:56 +0200

    feat(web): add Vercel Analytics to pages router in app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 import { HexagonBackground } from '@/components/ui/shadcn-io/hexagon-background';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <HexagonBackground className="min-h-screen">
         <Component {...pageProps} />
+        <Analytics />
       </HexagonBackground>
     </ThemeProvider>
   );
