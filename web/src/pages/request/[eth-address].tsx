c8e9b112e5907038be52fcc39de9a6dc674b95d4
plebeius <tom@plebbit.com>
2025-10-16 17:36:12 +0200

    fix(web): pass missing prop to RequestPage

--- web/src/pages/request/[eth-address].tsx
+++ web/src/pages/request/[eth-address].tsx
@@ -2,13 +2,17 @@ import { useRouter } from 'next/router';
 import type { GetServerSideProps } from 'next';
 import RequestPage from '../request';
 
-export default function RequestWithEthAddress() {
+type RequestWithEthAddressProps = {
+  isNorthAmerica: boolean;
+};
+
+export default function RequestWithEthAddress({ isNorthAmerica }: RequestWithEthAddressProps) {
   const router = useRouter();
   const { 'eth-address': ethAddress } = router.query as { 'eth-address'?: string };
-  return <RequestPage prefilledAddress={ethAddress || ''} />;
+  return <RequestPage prefilledAddress={ethAddress || ''} isNorthAmerica={isNorthAmerica} />;
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps<RequestWithEthAddressProps> = async (ctx) => {
   const countryHeader = (ctx.req.headers['x-vercel-ip-country'] as string) || '';
   const country = countryHeader.toUpperCase();
   const isNorthAmerica = country === 'US' || country === 'CA';
